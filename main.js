701b06953a4b269e34a6f7a07de71b0464c4f199
Michael Freeman <michael.k.freeman@gmail.com>
2017-09-26 15:38:41 -0700

    Add download link

--- main.js
+++ main.js
@@ -172,6 +172,20 @@ $("#file").change(function() {
 
 });
 
+// Download button
+function downloadCanvas(link, canvasId, filename) {
+    link.href = document.getElementById(canvasId).toDataURL();
+    link.download = filename;
+}
+
+/** 
+ * The event handler for the link's onclick event. We give THIS as a
+ * parameter (=the link element), ID of the canvas and a filename.
+ */
+document.getElementById('download').addEventListener('click', function() {
+    downloadCanvas(this, 'can-heroCanvas', 'triangle-image.png');
+}, false);
+
 // Wait for image to load
 $(window).on("load", function() {
     build();
