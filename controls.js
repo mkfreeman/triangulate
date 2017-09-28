7d41600bdab345fcd3d23f2b61e7243ebe251d24
Michael Freeman <michael.k.freeman@gmail.com>
2017-09-27 18:47:13 -0700

    Refactor controls

---
+++ controls.js
@@ -0,0 +1,98 @@
+// Change events
+$(function () {
+    // File uploader
+    $("#file")
+        .change(function () {
+            // New file reader
+            var reader = new FileReader();
+            // Empty the container -- a little lazy
+            $('.ele-container').empty();
+            $('canvas').remove();
+            reader.onloadend = function (e) {
+                img = $('#my-img');
+                img.attr('src', e.target.result);
+                img.one("load", function () {
+                    build();
+                })
+                    .each(function () {
+                        if (this.complete) 
+                            $(this).load();
+                        }
+                    );
+            };
+            reader.readAsDataURL(this.files[0]);
+        });
+
+    // Download button
+    function downloadCanvas(link, canvasId, filename) {
+        link.href = document
+            .getElementById(canvasId)
+            .toDataURL();
+        link.download = filename;
+    }
+
+    // Download
+    document
+        .getElementById('download')
+        .addEventListener('click', function () {
+            downloadCanvas(this, 'can-heroCanvas', 'triangle-image.png');
+        }, false);
+
+    // Blur slider
+    var blurSlider = document.getElementById('blur-slider');
+    var pointsSlider = document.getElementById('points-slider');
+    noUiSlider.create(blurSlider, {
+        start: BLUR_RADIUS,
+        animate: false,
+        step: 1,
+        range: {
+            min: 0,
+            max: 100
+        }
+    });
+
+    blurSlider
+        .noUiSlider
+        .on('set', function (value) {
+            BLUR_RADIUS = Math.floor(value);
+            drawBlur();
+            canvases.map(drawTriangle)
+        });
+
+    blurSlider
+        .noUiSlider
+        .on('update', function (value) {
+            $('#blur-slider-label').text('Blur: ' + Math.floor(value));
+        });
+
+    // Number of triangles slider
+    noUiSlider.create(pointsSlider, {
+        start: NUM_POINTS,
+        animate: false,
+        step: 1,
+        range: {
+            min: 10,
+            max: 10000
+        }
+    });
+
+    pointsSlider
+        .noUiSlider
+        .on('set', function (value) {
+            NUM_POINTS = Math.floor(value);
+            canvases.map(drawTriangle);
+        });
+
+    pointsSlider
+        .noUiSlider
+        .on('update', function (value) {
+            $('#points-slider-label').text('Num. Triangles: ' + Math.floor(value));
+        });
+
+    // Wait for image to load
+    $(window).on("load", function () {
+        build();
+        $('.modal').modal();
+    });
+    $('#points-slider-label').text('Num. Triangles: ' + NUM_POINTS);
+});
--- main.js
+++ main.js
@@ -1,35 +1,49 @@
-// Main.js Global variables
+// Global variables
 var height;
 var width;
 var BLUR_RADIUS = 0;
 var NUM_POINTS = 500;
 var img;
+var voronoi;
+
 
 // Function to build -- after image is uploaded
 var build = function() {
     // Select image
     img = document.getElementById('my-img');
-    var innerWidth = $(window).innerWidth()
-    width = innerWidth < 700 ? innerWidth * .9 : innerWidth * .4;
+    var innerWidth = $(window).innerWidth();
+    width = innerWidth < 700 ?
+        innerWidth * 0.9 :
+        innerWidth * 0.4;
     img.width = width;
     height = img.height;
 
-    // Make Canvas elements (for photos)
-    canvases.forEach(makeCanvas)
 
-    // Append arrow
+    // Define voronoi function
+    voronoi = d3
+        .voronoi()
+        .extent([
+            [-1, -1],
+            [
+                width + 1,
+                height + 1
+            ]
+        ]);
+    // Make Canvas elements (for photos)
+    canvases.forEach(makeCanvas);
+
+    // Append arrow on large screens
     if (innerWidth > 700) {
-        $('.ele-container').append('<span>&#x2192;</span>')
+        $('.ele-container').append('<span>&#x2192;</span>');
         $('.upload-text').text('Upload Image');
     } else {
         $('.upload-text').text('Take Photo');
     }
 
-
     // Blur Canvases
     drawBlur();
 
-    // Append svgs (for triangles)
+    // Append canvases for triangles
     appendCanvases();
     canvases.map(drawTriangle);
 };
@@ -42,12 +56,14 @@ var canvases = [{
 
 // Function to make Canvas elements
 var makeCanvas = function(can) {
-    console.log('make canvas ', img)
+    // Create canvas
     var canvas = document.createElement('canvas');
     canvas.id = can.id;
     canvas.className += can.className;
     canvas.width = width;
     canvas.height = height;
+
+    // Draw image
     canvas
         .getContext('2d')
         .drawImage(img, 0, 0, width, height);
@@ -55,9 +71,10 @@ var makeCanvas = function(can) {
         .getElementsByClassName('ele-container')[0]
         .appendChild(canvas);
 }
-// canvases.forEach(makeCanvas) Blur the canvas
+
+// Blur the initial image
 var drawBlur = function() {
-    $('#blur-slider-label').text("Blur: " + BLUR_RADIUS)
+    $('#blur-slider-label').text("Blur: " + BLUR_RADIUS);
     var canvas = document.getElementById('heroCanvas');
     canvas
         .getContext('2d')
@@ -65,6 +82,7 @@ var drawBlur = function() {
     stackBlurCanvasRGBA('heroCanvas', 0, 0, width, height, BLUR_RADIUS);
 };
 
+// Append canvas elements to draw in
 var appendCanvases = function() {
     canvases
         .forEach(function(can) {
@@ -74,165 +92,75 @@ var appendCanvases = function() {
                 .attr('width', width)
                 .attr('height', height)
                 .attr('id', 'can-' + can.id)
-                .attr("class", "triangles " + can.id)
+                .attr("class", "triangles " + can.id);
 
-        })
-}
+        });
+};
 
 // Function to get color from the triangle
 var getColor = function(d, c) {
+    // Get triangle center
     var x = 0;
     var y = 0;
     d.forEach(function(dd) {
         x += dd[0];
         y += dd[1];
-    })
+    });
     x = x / 3;
     y = y / 3;
-    var pixelData = document.getElementById('heroCanvas')
+
+    // Get color
+    var pixelData = document
+        .getElementById('heroCanvas')
         .getContext('2d')
         .getImageData(x, y, 1, 1)
         .data;
     var color = 'rgba(' + pixelData[0] + ',' + pixelData[1] + ',' + pixelData[2] + ',' + pixelData[3] + ')';
     return color;
-}
+};
+
 // Function to draw a cell
-function drawCell(cell, con) {
-    if (!cell || !con) return false;
+var drawCell = function(cell, con) {
+    if (!cell || !con)
+        return false;
+
+    // Draw path
     con.beginPath();
     con.moveTo(cell[0][0], cell[0][1]);
     for (var j = 1, m = cell.length; j < m; ++j) {
         con.lineTo(cell[j][0], cell[j][1]);
     }
 
+    // Fill path
     con.fillStyle = getColor(cell);
-    // con.fillStyle = 'green';
     con.strokeStyle = 'none';
     con.fill();
     con.closePath();
-    // con.stroke();
     return true;
 }
+
 // Function to draw triangles
 var drawTriangle = function(can) {
-    // var voronoi = d3.voronoi();
-    var sites = d3.range(NUM_POINTS)
+    // Sample points
+    var sites = d3
+        .range(NUM_POINTS)
         .map(function(d) {
-            return [Math.random() * width, Math.random() * height];
+            return [
+                Math.random() * width,
+                Math.random() * height
+            ];
         });
 
-    var voronoi = d3.voronoi()
-        .extent([
-            [-1, -1],
-            [width + 1, height + 1]
-        ]);
+    var polygons = voronoi(sites).triangles();
 
-    var diagram = voronoi(sites),
-        links = diagram.links(),
-        polygons = diagram.triangles();
-
+    // Clear canvas
     var canvas = document.getElementById("can-" + can.id);
-    var context = canvas.getContext("2d")
+    var context = canvas.getContext("2d");
     context.clearRect(0, 0, canvas.width, canvas.height);
 
-
     // Draw paths!
-    for (var i = 0, n = polygons.length; i < n; ++i) drawCell(polygons[i], context);
+    for (var i = 0, n = polygons.length; i < n; ++i) {
+        drawCell(polygons[i], context);
+    }
+
 };
-
-// Change events
-$('#blur-size input').val(BLUR_RADIUS)
-$('#num-points input').val(NUM_POINTS)
-$('#blur-size input').on('change', function(value) {
-    BLUR_RADIUS = this.value;
-    drawBlur();
-    canvases.map(drawTriangle)
-});
-
-// Number of triangle points
-$('#num-points input').on('change', function(value) {
-    NUM_POINTS = this.value;
-    canvases.map(drawTriangle)
-});
-
-
-// File uploader
-$("#file").change(function() {
-    // New file reader
-    var reader = new FileReader();
-    // Empty the container -- a little lazy
-    $('.ele-container').empty();
-    $('canvas').remove();
-    reader.onloadend = function(e) {
-        // img = document.getElementById('my-img');
-        img = $('#my-img');
-        // img.onload(function() {
-        //     build();
-        // })
-        img.attr('src', e.target.result);
-        img.one("load", function() {
-            build();
-        }).each(function() {
-            if (this.complete) $(this).load();
-        });
-        // img.src = e.target.result;
-    };
-
-    reader.readAsDataURL(this.files[0]);
-
-});
-
-// Download button
-function downloadCanvas(link, canvasId, filename) {
-    link.href = document.getElementById(canvasId).toDataURL();
-    link.download = filename;
-}
-
-/** 
- * The event handler for the link's onclick event. We give THIS as a
- * parameter (=the link element), ID of the canvas and a filename.
- */
-document.getElementById('download').addEventListener('click', function() {
-    downloadCanvas(this, 'can-heroCanvas', 'triangle-image.png');
-}, false);
-
-var blurSlider = document.getElementById('blur-slider');
-var pointsSlider = document.getElementById('points-slider');
-noUiSlider.create(blurSlider, {
-    start: BLUR_RADIUS,
-    animate: false,
-    step: 1,
-    range: {
-        min: 0,
-        max: 100
-    }
-});
-blurSlider.noUiSlider.on('set', function(value) {
-    BLUR_RADIUS = Math.floor(value);
-    drawBlur();
-    canvases.map(drawTriangle)
-});
-
-noUiSlider.create(pointsSlider, {
-    start: NUM_POINTS,
-    animate: false,
-    step: 1,
-    range: {
-        min: 10,
-        max: 10000
-    }
-});
-
-pointsSlider.noUiSlider.on('set', function(value) {
-    NUM_POINTS = Math.floor(value);
-    $('#points-slider-label').text('Num. Triangles: ' + NUM_POINTS);
-    canvases.map(drawTriangle)
-});
-
-// Wait for image to load
-$(window).on("load", function() {
-    build();
-    $('.modal').modal();
-});
-$(".button-collapse").sideNav();
-$('#points-slider-label').text('Num. Triangles: ' + NUM_POINTS);
